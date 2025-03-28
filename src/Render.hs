a0ba3d11ba25f7f733e2685e3715e681a5a5063c
Tomke <pfoch.tomke@gmail.com>
2025-03-28 23:44:47 +0100

    no monad clones today

--- src/Render.hs
+++ src/Render.hs
@@ -9,11 +9,12 @@ import Track (Track (..), Metadata (..), Attr (..), File (..))
 import Data.Map ((!), Map, lookup)
 import System.FilePath (combine)
 import System.Process (callCommand)
-import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Reader ( ReaderT (runReaderT), ask )
 import Control.Monad.Trans.Class (lift)
 import Data.List (intercalate)
 import Prelude hiding (lookup)
-import Data.Set
+import Data.Maybe (fromMaybe)
+import System.Directory (removeFile, renameFile)
 
 data RenderSettings = MergedRender{ -- TODO No seperation
     supported :: [Metadata],
@@ -32,43 +33,15 @@ data Task = Render String | UpdateMetadata FilePath String | Move FilePath Strin
 
 data Env = Env{settings :: RenderSettings, outDir :: FilePath, tracks :: [Track String]}
 
-data FFCtx = FFCtx{counter :: Int, selected :: [String], flags :: Set String, command :: String}
-data FFBuilder m = FFBuilder{_runBuilder :: (FFCtx) -> (FFCtx, m)}
+getSource :: ReaderT Env IO String
+getSource = do
+  trks <- fmap tracks ask
+  return $ case trks of
+    (trk:[]) -> "-i " ++ source trk ++ " -map 0:0"
+    _ -> concatMap (flip (++) "\" " . (++) "-i \"" . source) trks ++
+          concatMap (flip (++) ":a:0]" . (++) "[" . show) [0..length trks] ++
+          "concat=n="++show (length trks)++":v=0:a=1[outa] -map \"[outa]\""
 
-instance Functor FFBuilder where
-    fmap :: (a -> b) -> FFBuilder a -> FFBuilder b
-    fmap f (FFBuilder g) = FFBuilder $ \s -> fmap f . g $ s
-
-instance Monad FFBuilder where
-  (>>=) :: FFBuilder a -> (a -> FFBuilder b) -> FFBuilder b
-  f >>= g = FFBuilder $ \s -> let (is, iv) = _runBuilder f s in _runBuilder (g iv) is
-
-addCmd :: String -> FFBuilder ()
-addCmd append = FFBuilder $ \c -> (c{command= command c ++ " " ++ append}, ())
-addSrc :: FilePath -> FFBuilder ()
-addSrc src = FFBuilder $ \c -> (c{counter=counter c + 1,
-                                  command=command c ++" -i \""++ src ++"\""}, ())
-select :: FFBuilder ()
-select = FFBuilder $ \c-> (c{selected = show (counter c -1) : selected c }, ())
-setFlag :: String -> FFBuilder ()
-setFlag flag = FFBuilder $ \c ->
-  let f = flags c
-      a = if flag `elem` f then " --" ++ flag else "" in
-    (c{flags = insert flag f, command = command c ++ a},())
-   
-concat :: FFBuilder ()
-concat = FFBuilder $
-  \(FFCtx i s f c) -> let nOut = "[o"++show i ++ "]" in
-    (FFCtx (i+1) [nOut] f (c ++" \""++ fiterCat s ++ nOut++"\""),())
-  where fiterCat inps = concatMap (flip (++) ":a:0]" . (++) "[" . show) inps ++
-          "concat=n="++show (length inps)++":v=0:a=1"
-mapSelected :: FFBuilder ()
-mapSelected = FFBuilder $
-  \c -> (c{selected = [], command = command c ++ mapOut (selected c)},())
-  where mapOut = concatMap (flip (++) "\"" . (++) " -map \"")
-    
-runFFBuilder :: FilePath -> FFBuilder a -> String
-runFFBuilder out b = command . fst . flip _runBuilder (FFCtx 0 [] empty "") $ b>>mapSelected>>addCmd ("\"" ++ out ++ "\"")
 
 getOutput :: ReaderT Env IO FilePath
 getOutput = do
@@ -77,14 +50,13 @@ getOutput = do
   trks <- fmap tracks ask
 
   let name = case cfg of
-        (MergedRender _ _ _) -> flip (!) (Attr Album) . metadata . head $ trks;
-        _ -> liftA2 (++) (concatMap (flip (++) ". " . flip (!) (Attr Nr) . metadata))
-                         (intercalate "_" . Prelude.map (flip (!) (Attr Title) . metadata)) trks;
-
+              (MergedRender _ _ _) -> flip (!) (Attr Album) . metadata . head $ trks
+              _ -> liftA2 (++) (concatMap (flip (++) ". " . flip (!) (Attr Nr)))
+                              (intercalate "_" . Prelude.map (flip (!) (Attr Title))) . Prelude.map metadata $ trks
   return . combine dir $ name ++ "." ++ format cfg
 
 
-getMetadata :: Metadata -> ReaderT Env FFBuilder ()
+getMetadata :: Metadata -> ReaderT Env IO FilePath
 getMetadata (File Cover) = do
   trks <- fmap tracks ask
   return $ case lookup (File Cover) . metadata . head $ trks of
@@ -97,42 +69,40 @@ getMetadata (File Video) = do
     (Just video ) -> "-i " ++ video ++ " -map " ++ show (length trks)
          ++ ":v:0  "
     Nothing -> ""
--- getMetadata (File Video) = do
+getMetadata (Attr a) = do
+  mtdt <- fmap (metadata . head . tracks) ask
+  let res = \attr -> "-metadata:s:v " ++ show a ++ "=\"" ++ attr ++ "\""
+  return . fromMaybe "" . fmap res . Data.Map.lookup (Attr a) $ mtdt
   
 
 ffrender :: ReaderT Env IO FilePath
 ffrender = do
-  env <- ask
-  out <- getOutput
-  let cmd = runFFBuilder out . flip runReaderT env . sequence $
-            [lift . mapM (flip select >> addSrc . source) . tracks $ env, -- insert Tracks
-             
-             ]
-  
-    
   src <- getSource
-  lift . callCommand $ "ffmpeg " ++ src ++ out
+  mtdt <- (=<<) (fmap concat . mapM getMetadata) . fmap (supported . settings) $ ask
+  out <- getOutput
+  lift . liftA2 (>>) putStrLn callCommand $ "ffmpeg " ++ src ++ " " ++ mtdt ++ " " ++ out
   return out
 
 ffupdate :: FilePath -> ReaderT Env IO FilePath
 ffupdate from = do
   out <- fmap outDir ask
-  src <- getSource
+  mtdt <- (=<<) (fmap concat . mapM getMetadata) . fmap (supported . settings) $ ask
   lift $ do
-    putStrLn $ "ffmpeg -i " ++ from ++ " -c copy -s " ++ show (metadata trk) ++ " -> " ++ out
-    putStrLn $ "delete " ++ from
+    liftA2 (>>) putStrLn callCommand $ "ffmpeg -i " ++ from ++ " -c copy " ++ mtdt ++ " " ++ out
+    removeFile from
     return out
 
 move :: FilePath -> ReaderT Env IO FilePath
 move from = do
-  out <- fmap outDir ask
+  to <- fmap outDir ask
   lift $ do
-    putStrLn $ "move " ++ from ++ " " ++ out
-    return out
+    putStrLn $ "Move " ++ from ++ " to " ++ to
+    renameFile from to
+    return to
 
 render :: Map String (Track String) -> FilePath -> RenderSettings -> [Task] -> IO [(String, FilePath)]
-render trkList outDir cfg@(SingleRender _ _ _) tasks =
-  mapM ((sequence) (Env cfg outDir . return . (!) trkList . fst) . exec) tasks
+render trkList outDir cfg@(SingleRender _ _ _) tasks = 
+  mapM ((\(trk, path)->sequence (trk, runReaderT path $ Env cfg outDir [trkList!trk])) . exec) tasks
  
   where exec (Render trk) = (trk, ffrender)
         exec (UpdateMetadata from trk) = (trk, ffupdate from)
